cfad481350570ec38edc682ddb910c9103b54a59
mrxpert <mrxpert15@gmail.com>
2019-06-09 14:29:38 +0500

    Defines path to static files

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const db = require('./db');
+const path = require('path');
 
 let app = express();
 
@@ -14,3 +15,4 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 app.use('/genres', require('./routes/genres'));
+app.use('/', express.static(path.join(__dirname, '../dist')));
